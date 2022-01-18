1434b9f5d49e9d69f5618dfb7766297cc00831a6
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2022-01-17 18:44:28 -0600

    badges should show for two test

--- Develop/ReadMe2.md
+++ Develop/ReadMe2.md
@@ -1,6 +1,6 @@
-#  What's the name of your repository 
+# jdjd 
 ##Description
-? Describe why did you build this project? 
+jdjddjdj jdjd jdj 
 ## Table of Contents:
 * [Installation](#installation)
 * [Usage](#License)
@@ -9,23 +9,26 @@
 * [Questions](#questions)
 ### Installation:
 In order to install the necessary dependencies, open the console and run thr following:
-```steps required ```
+jdjd
 ### Usage:
 Provide instructions and examples for use. Include screenshots as needed.
-screenshots 
-### Credits
-List your collaborators, if any, with links to their GitHub profiles.
-callaborators 
+djd
 ### License:
 This project is Licensed under:
-MIT License i
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 ### Features
 If your project has a lot of features, list them here.
 ### How to Contribute:
-crystal 
+jdjd
 ### Tests:
 In order to test open the console and run the following:
-```dd```
+undefined
+### Credits
+List your collaborators, if any, with links to their GitHub profiles.
+jdj
+### Author
+The author name is
+jdjd
 ### Questions:
-If you have any questions contact me on [GitHub](https://github.com/Kristal4673) or contact
-Please provide information your application repository?  at what is your email 
+If you have any questions contact me on [GitHub](https://github.com/undefined) or contact
+jdjd at jdjd
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseType = license.license; 
+  let licenseType = license; 
   let yourLicense = ''
-  if(licenseType === 'MIT') {
-    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  } else if (licenseType === 'GPLv3') {
-    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-  } else if (licenseType === 'GPL') {
-    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  if (licenseType === "MIT License i") {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (licenseType === "GNU GPLv3 ") {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (licenseType === "GPL") {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`;
   } else {
-    license.license = "N/A"
+    license.license = "N/A";
   }
   return yourLicense;
 };
@@ -50,20 +50,20 @@ ${data.description}${data.learn} ${data.motivation} ${data.solve}
 * [Questions](#questions)
 ### Installation:
 In order to install the necessary dependencies, open the console and run thr following:
-\`\`\`${data.installations}\`\`\`
+${data.installations}
 ### Usage:
 Provide instructions and examples for use. Include screenshots as needed.
 ${data.usage}
 ### License:
 This project is Licensed under:
-${data.license}
+${renderLicenseBadge(data.license)}
 ### Features
 If your project has a lot of features, list them here.
 ### How to Contribute:
 ${data.contribution}
 ### Tests:
 In order to test open the console and run the following:
-\`\`\`${data.test}\`\`\`
+${data.test}
 ### Credits
 List your collaborators, if any, with links to their GitHub profiles.
 ${data.credits}
